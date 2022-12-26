0bdc35ad47a9a87bddab91230fde486cee746bd7
Rafael Henrique <spectggof@gmail.com>
2022-12-26 14:17:12 -0300

    Updating to route listing

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
 
 import express from "express";
-import "./src/database/index.js";
-import { routesBooks } from "./src/routes/book.js";
-import { routesUsers } from "./src/routes/user.js";
+import "./src/database/index";
+
+import { routesBooks } from "./src/routes/book";
+import { routesUser } from "./src/routes/user";
+import { routesToken } from "./src/routes/token";
+import { routesAdmin } from "./src/routes/admin";
 
 dotenv.config();
 
@@ -12,6 +15,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routesBooks);
-app.use(routesUsers);
+app.use(routesUser);
+app.use(routesToken);
+app.use(routesAdmin);
 
 export default app;
